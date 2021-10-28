959658122fb6b411a68699017e403fd35ee02ee3
User <shafaisal.m@pacewisdom.com>
2021-10-28 20:25:40 +0530

    first commit

---
+++ app/src/main/java/com/example/agecalculator/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.agecalculator
+
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import com.google.android.material.button.MaterialButton
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val button = findViewById<Button>(R.id.btnDatePicker)
+        button.setOnClickListener { view ->
+            clickDatePicker(view)
+        }
+    }
+
+
+    fun clickDatePicker(view: View) {
+        var calemdar = Calendar.getInstance()
+        val year = calemdar.get(Calendar.YEAR)
+        val month = calemdar.get(Calendar.MONTH)
+        val day = calemdar.get(Calendar.DAY_OF_MONTH)
+
+
+        DatePickerDialog(
+            this, DatePickerDialog.OnDateSetListener { view, SelectedYear, SelectedMonth, SelectedDayOfMonth ->
+
+               val selectedDate="$SelectedYear/${SelectedMonth+1}/$SelectedDayOfMonth"
+                val sdf=SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                val theDate=sdf.parse(selectedDate)
+
+                var textView = findViewById<TextView>(R.id.tvSelectedDate)
+                textView.text=selectedDate
+
+
+
+
+            },
+            year, month, day
+
+
+        ).show()
+    }
+}
+
+
